22728359cb6cb3fd2e3c8564410b26692434ae8e
alaoks <493088943@qq.com>
2022-05-07 09:54:43 +0800

    首个版本

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,64 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <div style="margin-left: 45%;">
+    <div>
+      <b>行列数：</b><input v-model.number="gridCount">
+    </div>
+    <div>
+      <div>在对应格子输入数据</div>
+      <div v-for="r in gridCount">
+        <div class="in" v-for="rd in gridCount" style="display: inline-block;">
+          <input style="width: 40px;"  v-model.number="gridValues[pos(r, rd)]">
+        </div>
+      </div>
+    </div>
+    <div>
+      <b>边框厚度:</b><input type="range" min="1" max="10" v-model.number="border"><span>{{border}}</span>
+    </div>
+    <div>
+      <b>单元格大小:</b><input type="range" min="40" max="200" v-model.number="gridSize"><span>{{gridSize}}</span>
+    </div>
+    <table>
+      <tr v-for="r in gridCount">
+        <td v-for="rd in gridCount">{{ gridValues[pos(r, rd)] }}</td>
+      </tr>
+    </table>
+  </div>
 </template>
 
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref, watch } from 'vue'
+const gridCount = ref(3)
+const gridValues = ref(new Array(9).fill(0))
 
-// This starter template is using Vue 3 experimental <script setup> SFCs
-// Check out https://github.com/vuejs/rfcs/blob/master/active-rfcs/0040-script-setup.md
+watch(gridCount,(newCount) => {
+  gridValues.value = new Array(newCount*newCount).fill(0)
+})
+
+const border = ref(1)
+const gridSize = ref(40)
+
+function pos(i, j) {
+  return (i - 1) * gridCount.value + (j - 1)
+}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+table {
+  margin-top: 40px;
+  border-collapse: collapse;
+  border-spacing: 0;
+}
+
+table td {
+  border: solid black;
+  border-width: v-bind('border+"px"');
+  margin: 0;
+  width: v-bind('gridSize+"px"');
+  height: v-bind('gridSize+"px"');
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+.in {
+ width: 40px;
+ margin-right: 10px;
 }
 </style>
